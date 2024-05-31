71233887a5535bafe4ac56ece805d27d5b87e41d
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-05-31 19:00:24 +0700

    change alert

--- src/DashboardLayout.jsx
+++ src/DashboardLayout.jsx
@@ -24,7 +24,7 @@ const DashboardLayout = ({ children }) => {
       setIsLoggedIn(true);
     } else {
       setIsLoggedIn(false);
-      alert('Login Terlebih Dahulu');
+      alert('Silakan Login Terlebih Dahulu');
       navigate('/login');
     }
   }, []);
